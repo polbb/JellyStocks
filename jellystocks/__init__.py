fecddfb7cb828eb9b83d1af511137566752de3af
polbb <paulbessone@gmail.com>
2023-11-16 23:14:44 +0000

    Updates

--- jellystocks/__init__.py
+++ jellystocks/__init__.py
@@ -1,5 +1,6 @@
+# jellystocks/__init__.py
 # include a comment at the beginning of the modified MATLAB code files, referencing the original license and
 # providing a link to the LICENSE file.
 
-from jellystocks.jellyfish_search import JellyfishOptimizer
-from portfolio import Portfolio
+from .jellyfish_search import JellyfishOptimizer
+from .portfolio import Portfolio
